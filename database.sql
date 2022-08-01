e8720d380f246c050a69a8da66206740c41ba8c7
FarooqKollect <farooq.kollect.systems@gmail.com>
2022-08-01 09:52:17 +0800

    database sql update

--- database.sql
+++ database.sql
@@ -4,3 +4,14 @@ CREATE TABLE todo(
     todo_id SERIAL PRIMARY KEY,
     description VARCHAR(255)
 );
+
+CREATE TABLE users(
+    user_id serial PRIMARY KEY,
+    user_name VARCHAR (255),
+	account_number VARCHAR ( 100 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+    user_role VARCHAR ( 255 ) NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+    last_login TIMESTAMP 
+);
